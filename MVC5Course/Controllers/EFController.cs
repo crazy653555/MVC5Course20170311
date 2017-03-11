f270d99a7fe6d88f277477703d6b0baa8a13271c
eason <crazy653555@gmail.com>
2017-03-11 17:09:03 +0800

    16 練習透過 Entity Framework 提供的物件服務與 LINQ 語法讀取資料

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -11,17 +11,20 @@ namespace MVC5Course.Controllers
     public class EFController : Controller
     {
         // GET: EF
+        FabricsEntities db = new FabricsEntities();
         public ActionResult Index()
         {
-            var db = new FabricsEntities();
-            db.Product.Add(new Product
+
+            var product = new Product
             {
                 ProductName = "BMW",
                 Price = 2,
                 Stock = 1,
                 Active = true
-            });
+            };
 
+            db.Product.Add(product);
+            
             try
             {
                 db.SaveChanges();
@@ -40,8 +43,16 @@ namespace MVC5Course.Controllers
                 throw;
             }
 
-            var data = db.Product.OrderByDescending(item => item.ProductId).ToList();
+            var pkey = product.ProductId;
+            
+            var data = db.Product.OrderByDescending(item => item.ProductId).Take(15);            
+             
+            return View(data);
+        }
 
+        public ActionResult Detail(int id)
+        {
+            var data = db.Product.FirstOrDefault(p => p.ProductId == id);
             return View(data);
         }
     }
