7f3ca99b126697a990738246f213c98a63d9abc7
eason <crazy653555@gmail.com>
2017-03-22 22:41:25 +0800

    25 練習實作 BaseController 共用控制器 ( 要設定 abstract 與自訂 HandleUnknownAction )

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -9,7 +9,7 @@ using System.Data.Entity;
 
 namespace MVC5Course.Controllers
 {
-    public class EFController : Controller
+    public class EFController : BaseController
     {
         // GET: EF
         FabricsEntities db = new FabricsEntities();
